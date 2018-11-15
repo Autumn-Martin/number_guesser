2dbb6bd2452b90028027b926fb7b7c3dfa57be1b
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-15 13:16:59 -0700

    Progress on initial game logic, functions, & variables

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-function guessDisplayer() {
-  var x = document.getElementById("userGuess").value;
-          document.getElementById("guessDisplay").innerHTML = x;
+function displayGuess() {
+  var guessInput = Math.round(Number(document.getElementById('guessInput').value));
+  document.getElementById('guessDisplay').innerHTML = guessInput;
 }
+
+let randNum = Math.floor(Math.random() * 11);
+const gameResponse = document.querySelector('.gameResponse')
+
+function checkGuess() {
+  let playerGuess = Math.round(Number(guessInput.value));
+  if (playerGuess === randNum) {
+    gameResponse.textContent = 'BOOM!';
+  } else {
+    gameResponse.textContent = 'Guess again!';
+  }
+}
+
+guessSubmission.addEventListener('click', checkGuess);
+guessSubmission.addEventListener('click', displayGuess);
