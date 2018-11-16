4f0f4b4a527be08f596ac5b471cd8ac351ebde44
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-15 21:20:23 -0700

    Disable clear button when it is not needed, organize, & change gameResponse to an id

--- main.js
+++ main.js
@@ -1,28 +1,15 @@
-// ---- display last guess ----
-function displayGuess() {
-  if (document.getElementById('guessInput').value === '') {
-    document.getElementById('guessDisplay').innerHTML = 'nada.'
-  } else if (isNaN(Number(document.getElementById('guessInput').value))) {
-    document.getElementById('guessDisplay').innerHTML = 'invalid'
-  } else {
-    var guessInput = parseInt(document.getElementById('guessInput').value);
-    document.getElementById('guessDisplay').innerHTML = guessInput;
-  }
-}
-
-guessSubmission.addEventListener('click', checkGuess);
 // ---- check guess ----
 var maxNumValue = 10
 var minNumValue = 1
 let randNum = Math.floor(Math.random() * (maxNumValue + 1));
-const gameResponse = document.querySelector('.gameResponse')
+const gameResponse = document.getElementById('gameResponse')
 
 function checkGuess() {
   document.getElementById('guessIntro').innerHTML = 'Your last guess was';
   document.getElementById('reset').style.display = 'inline';
 
   let playerGuess = parseInt(guessInput.value);
-  
+
   if (playerGuess === randNum) {
       gameResponse.textContent = 'BOOM!';
   } else if ((guessInput.value === '') || (isNaN(Number(guessInput.value)))) {
@@ -43,19 +30,49 @@ function checkGuess() {
   }
 }
 
+guessSubmission.addEventListener('click', checkGuess);
+
+// ---- display last guess ----
+function displayGuess() {
+  if (document.getElementById('guessInput').value === '') {
+    document.getElementById('guessDisplay').innerHTML = 'nada.'
+  } else if (isNaN(Number(document.getElementById('guessInput').value))) {
+    document.getElementById('guessDisplay').innerHTML = 'invalid'
+  } else {
+    var guessInput = parseInt(document.getElementById('guessInput').value);
+    document.getElementById('guessDisplay').innerHTML = guessInput;
+  }
+}
+
 guessSubmission.addEventListener('click', displayGuess);
 
-// ---- clear input field ----
-var clearButton = document.querySelector('#clear');
+// ---- clear input field & disable clear button ----
+var clearButton = document.getElementById('clear');
 
 clearButton.addEventListener('click', clearInput);
 
 function clearInput() {
   document.getElementById('guessInput').value = '';
+  disableButton();
 }
 
+function disableButton() {
+  document.getElementById('clear').disabled = true
+  document.getElementById('clear').style.backgroundColor = '#D0D2D3'
+}
+
+// ---- enable clear button when user begins typing ----
+function enableButton() {
+  document.getElementById('clear').disabled = false
+  document.getElementById('clear').style.backgroundColor = '#929497'
+}
+
+var guessInput = document.getElementById('guessInput')
+
+guessInput.addEventListener('keypress', enableButton)
+
 // ---- reset game ----
-var resetButton = document.querySelector('#reset');
+var resetButton = document.getElementById('reset');
 
 resetButton.addEventListener('click', resetGame);
 
